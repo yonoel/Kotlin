dd5f1d345db10771d68a6e5c129b2024a6c527d0
qianyizhen <yonoel@icloud.com>
2021-05-23 10:45:13 +0800

    init

---
+++ kotlin-demo-a/src/main/kotlin/me/yonoel/demoa/ATest.kt
@@ -0,0 +1,13 @@
+package me.yonoel.demoa
+
+/**
+ * .
+ * @author yonoel 2021/05/13
+ */
+open class ATest {
+    open fun hello() = "String"
+}
+
+fun main() {
+    ATest().hello()
+}
---
+++ kotlin-demo-b/src/main/kotlin/me/yonoel/BTest.kt
@@ -0,0 +1,12 @@
+package me.yonoel
+
+import me.yonoel.demoa.ATest
+
+/**
+ * .
+ * @author yonoel 2021/05/13
+ */
+class BTest {
+    fun test(){
+    }
+}
